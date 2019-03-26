707f849832ad83b3f82dcfb60320d60728522b5d
tomginsberg <tommyginsberg@gmail.com>
2019-03-26 14:21:30 -0700

    all

--- SolarSysSim/sim.py
+++ SolarSysSim/sim.py
@@ -1,14 +1,43 @@
 from p5 import *
 from solar_sys_parameters import *
+from random import randint
+import numpy as np
 
 def setup():
+    global trajectory
     size(1000, 800)
-    no_stroke()
+    no_stroke()    
+    # trajectory = PShape(visible=True, stroke_color=(255,255,255), attribs="path")
+    # trajectory.add_vertex((100,100))
+    # trajectory.add_vertex((300,300))
+
+
+def to_cart(r,t):
+    return (r*np.cos(t),r*np.sin(t))
+
+t_x = 200
+t_y = 0
+traj_max = 200
+trajectory = [(0,0) for _ in range(traj_max)]
+traj_index = 0
+trajectory[traj_index] = to_cart(t_x,t_y)
+traj_index += 1
+
+#def compute_lambert_arc(r0,rf,t):
+
 
 def draw():
-    background(0,0,0,100)
+    global trajectory, t_x, t_y, traj_index, traj_max
+    background(0,0,0)
+    t_y += 2*np.pi/200
+    t_x -= 0.5
+    trajectory[traj_index % traj_max] = to_cart(t_x,t_y)
+    traj_index += 1
     with push_matrix():
         translate(width/2, height/2)
+        fill(255,255,255)
+        for p in trajectory:
+            circle(p,2)
         fill(253, 184, 19)
         circle((0, 0), sun.s)
         fill(255,255,255)
---
+++ lambert_solver.py
@@ -0,0 +1,33 @@
+import numpy as np 
+from poliastro.core.iod import izzo as izzo_fast
+
+
+class Planet:
+    #################  AU     AU   Years  Rads  GM (AU^3/Year^2)
+    def __init__(self,orbit, size, period, phi, attractor):
+        self.orbit = orbit
+        self.period = period
+        self.omega = 2 * np.pi / period
+        self.size = size
+        self.phi = phi
+    def get_pos(self,t):
+        return np.array([self.orbit * np.cos(self.omega * t + self.phi), self.orbit * np.sin(self.omega * t + self.phi), 0])
+        
+
+##AU
+earth_radius = 0.00004258756
+##AU^3/year^2
+earth_attractor = 0.0001184
+
+#The sun only has one parameter of interest, it's attractor 
+Sun = 39.42
+
+Earth = Planet(1, earth_radius, 1, 0, earth_attractor)
+Mercury = Planet(0.374496, earth_radius * 0.38, 0.241, np.pi/2, 0.0552734 * earth_attractor)
+Mars = Planet(1.5458, earth_radius * 0.53, 1.8821, np.pi + .7, 0.107447 * earth_attractor)
+Venus = Planet(0.726088, earth_radius * 0.95, 0.6156, 3 * np.pi/4, 0.814996 * earth_attractor)
+Jupiter = Planet(5.328, earth_radius * 3, 11.87, np.pi/4 + .38, 317.828 * earth_attractor)
+
+lambert_sol = izzo_fast(Sun, Earth.get_pos(0), Earth.get_pos(1), 1, M=0, numiter=10, rtol=1e-2)
+for v0, v in lambert_sol:
+    print("V0: {}, Vf: {}".format(v0[:2], v[:2]))
