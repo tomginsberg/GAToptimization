a7eaecca06774bf1e8c5b6a6d3634f39a5317665
tomginsberg <tommyginsberg@gmail.com>
2019-03-26 20:55:23 -0700

    Stuff

--- MATLAB Lambert Solver/Planet.m
+++ MATLAB Lambert Solver/Planet.m
@@ -8,6 +8,13 @@ classdef Planet
     end
     methods
         function obj = Planet(orbit_radius, planet_radius,period,attractor,phase)
+            obj.orbit_radius = orbit_radius;
+            obj.planet_radius = planet_radius;
+            obj.period = period;
+            obj.attractor = attractor;
+            obj.phase = phase;
+        end
+        
         function [x, y] = get_pos(obj, time)
             x = obj.orbit_radius * cos(obj.phase + 1/obj.period * 2*pi * time);
             y = obj.orbit_radius * sin(obj.phase + 1/obj.period * 2*pi * time);
