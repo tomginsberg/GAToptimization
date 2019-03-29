a56b8ae559a4d8384556dfdc02b4e94884582f3f
tomginsberg <tommyginsberg@gmail.com>
2019-03-28 17:57:31 -0700

    Comet

--- lambert_solver_slow.py
+++ lambert_solver_slow.py
@@ -1,6 +1,6 @@
 import numpy as np
 from poliastro.core.iod import izzo as izzo_fast
-
+from comet import Comet
 
 class Planet:
     # AU     AU   Years  Rads  GM (AU^3/Year^2)
@@ -27,8 +27,13 @@ Earth = Planet(1, earth_radius, 1, 0, earth_attractor)
 Mercury = Planet(0.374496, earth_radius * 0.38, 0.241, np.pi / 2, 0.0552734 * earth_attractor)
 Mars = Planet(1.5458, earth_radius * 0.53, 1.8821, np.pi + .7, 0.107447 * earth_attractor)
 Venus = Planet(0.726088, earth_radius * 0.95, 0.6156, 3 * np.pi/4, 0.814996 * earth_attractor)
-Jupiter = Planet(5.328, earth_radius * 3, 11.87, np.pi / 4 + .38, 317.828 * earth_attractor)
+Jupiter = Planet(5.328, earth_radius * 3, 11.87, np.pi / 2 + .38, 317.828 * earth_attractor)
 
-lambert_sol = izzo_fast(Sun, Earth.get_pos(0), Earth.get_pos(1/4), 1/4, M=0, numiter=10, rtol=1e-2)
-for v0, v in lambert_sol:
-    print("V0: {}, Vf: {}".format(v0[:2], v[:2]))
+# lambert_sol = izzo_fast(Sun, Earth.get_pos(0), np.array([-6.74393, -0.197631,0]), 3, M=0, numiter=10, rtol=1e-2)
+# for v0, v in lambert_sol:
+#     print("V0: {}, Vf: {}".format(v0[:2], v[:2]))
+
+Comet = Comet()
+print(Comet.get_pos(0))
+for i in range(100):
+    print(Comet.get_pos(i))
