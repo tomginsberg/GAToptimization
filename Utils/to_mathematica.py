ddbb8cc875983b2fb6fe24efb4727ff1d952de54
tomginsberg <tommyginsberg@gmail.com>
2019-03-28 19:47:21 -0700

    mathematica code gen to textfile

--- Utils/to_mathematica.py
+++ Utils/to_mathematica.py
@@ -17,4 +17,10 @@ def convert(pos, times, vels):
     print(posStr[:-2]+close)
     print(timeStr[:-2]+close)
     print(velStr[:-2]+close)
+    with open("trajectory_data.txt", "a") as f:
+        f.write("\n"+posStr[:-2]+close+"\n")
+        f.write(timeStr[:-2]+close+"\n")
+        f.write(velStr[:-2]+close+"\n")
+
+convert([[1,0],[0,1]], [0,2],[[1,1]])
 
