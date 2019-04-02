540354dc1223682285221710f4ad4a47f1196d4e
tomginsberg <tommyginsberg@gmail.com>
2019-04-01 19:17:06 -0700

    data logger

--- Utils/logger.py
+++ Utils/logger.py
@@ -1,6 +1,14 @@
 import csv 
 
 def log(t0, tf, phis):
+    """
+    import Utils.logger as logger
+
+    t = trajectory  #do fancy computations 
+
+    logger.log(t.t0, t.tf, t.phases)
+    #and your good
+    """
     a = [["Log Data"],[t0,tf], phis]
     with open("new_file.csv","w+") as my_csv:
         csvWriter = csv.writer(my_csv,delimiter=',')
