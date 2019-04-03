7ce7427a3c8ed2a96c4b1638b747bc827246a95a
tomginsberg <tommyginsberg@gmail.com>
2019-04-02 17:41:33 -0700

    getter

---
+++ get_trajectory.py
@@ -0,0 +1,13 @@
+import sys
+
+print("Generating Trajectory to {}".format(sys.argv[1]))
+#Create trajectory to sys.argv[1]
+
+#Call the logger
+
+#Call convert to mathematcia
+
+#Call jeremy code
+#Wait for it to be done
+
+print("Done")
